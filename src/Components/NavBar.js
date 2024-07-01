cbf791d841fcafafdbe8a854109abdcc83dd5f16
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-01 11:39:19 -0400

    Set up Navbar with routes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,35 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 function NavBar() {
   return (
     <div className="NavBar">
-      <h1>NavBar</h1>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Dnd Character Manager
+          </a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Classes
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/spells">
+                  Spells
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
