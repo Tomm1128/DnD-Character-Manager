a60898ccf750d55ff7f64317f7ff22834ce45194
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-01 15:58:45 -0400

    Control form inputs with state

--- src/Components/CreateCharacterForm.js
+++ src/Components/CreateCharacterForm.js
@@ -1,148 +1,144 @@
 import React from "react"
+import CharacterInfoForm from "./CharacterInfoForm"
 
-function CreateCharacterForm() {
+function CreateCharacterForm({ formData, updateFormData }) {
   return (
     <div className="create-character-form container mt-5">
       <h1>Create Character Form</h1>
       <form>
-        <h3>Character Information</h3>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Name:</label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Name" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Height:</label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Height" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Weight:</label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Weight" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Alignment:</label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="True Neutral"
-            />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Languages:</label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Common" />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Level:</label>
-          <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={0} />
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Race:</label>
-          <div className="col-sm-10">
-            <select className="form-control">
-              <option>Select Race</option>
-              <option>Dwarf</option>
-              <option>Elf</option>
-              <option>Halfling</option>
-              <option>Human</option>
-              <option>Dragonborn</option>
-              <option>Gnome</option>
-              <option>Half-Elf</option>
-              <option>Half-Orc</option>
-              <option>Tiefling</option>
-            </select>
-          </div>
-        </div>
-        <div className="form-group row">
-          <label className="col-sm-2 col-form-label">Class:</label>
-          <div className="col-sm-10">
-            <select className="form-control">
-              <option>Select Class</option>
-              <option>Barbarian</option>
-              <option>Bard</option>
-              <option>Cleric</option>
-              <option>Druid</option>
-              <option>Fighter</option>
-              <option>Monk</option>
-              <option>Paladin</option>
-              <option>Ranger</option>
-              <option>Rogue</option>
-              <option>Sorcerer</option>
-              <option>Warlock</option>
-              <option>Wizard</option>
-            </select>
-          </div>
-        </div>
+        {
+          <CharacterInfoForm
+            formData={formData}
+            updateFormData={updateFormData}
+          />
+        }
         <h3>Ability Scores</h3>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Strength:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="strength"
+              value={formData.strength}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Dexterity:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="dexterity"
+              value={formData.dexterity}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Constitution:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="constitution"
+              value={formData.constitution}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Intelligence:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="intelligence"
+              value={formData.intelligence}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Wisdom:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="wisdom"
+              value={formData.wisdom}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Charisma:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={10} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={10}
+              name="charisma"
+              value={formData.charisma}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <h3>Role Play Stats</h3>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Ideals:</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Ideals" />
+            <textarea
+              className="form-control"
+              placeholder="Write your character's Ideals"
+              name="ideals"
+              value={formData.ideals}
+              onChange={updateFormData}
+            ></textarea>
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Bonds:</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Bonds" />
+            <textarea
+              className="form-control"
+              placeholder="Write about your character's Bonds"
+              name="bonds"
+              value={formData.bonds}
+              onChange={updateFormData}
+            ></textarea>
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Flaws:</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" placeholder="Flaws" />
+            <textarea
+              className="form-control"
+              placeholder="Write your character's Flaws"
+              name="flaws"
+              value={formData.flaws}
+              onChange={updateFormData}
+            ></textarea>
           </div>
         </div>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Inspiration Points:</label>
           <div className="col-sm-10">
-            <input type="number" className="form-control" placeholder={0} />
+            <input
+              type="number"
+              className="form-control"
+              placeholder={0}
+              name="inspirationPoints"
+              value={formData.inspirationPoints}
+              onChange={updateFormData}
+            />
           </div>
         </div>
         <div className="form-group row">
@@ -152,7 +148,24 @@ function CreateCharacterForm() {
               type="text"
               className="form-control"
               placeholder="Background"
+              name="background"
+              value={formData.background}
+              onChange={updateFormData}
             />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="col-sm-2 col-form-label">
+            Additional Background Notes:
+          </label>
+          <div className="col-sm-10">
+            <textarea
+              className="form-control"
+              placeholder="Add more details to your character's background if you would like (optional)"
+              name="additionalBackgroundNotes"
+              value={formData.additionalBackgroundNotes}
+              onChange={updateFormData}
+            ></textarea>
           </div>
         </div>
       </form>
