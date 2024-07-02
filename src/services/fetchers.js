a6dfb3c86e99c28cac58624d3605fd945c112e06
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-02 11:23:44 -0400

    Post new character to db with ids

--- src/services/fetchers.js
+++ src/services/fetchers.js
@@ -1,15 +1,33 @@
 const data = "http://localhost:3000/playerCharacters/"
 const url = "https://www.dnd5eapi.co/api/"
 
-const createCharacter = (newCharacter) => {
+const getCharacters = () => {
+  return fetch(data).then((resp) => resp.json())
+}
+
+const idCheck = async () => {
+  const currentCharacters = await getCharacters()
+  if (currentCharacters.length === 0) {
+    return "1"
+  } else {
+    const lastEntry = currentCharacters[currentCharacters.length - 1]
+    const newId = parseInt(lastEntry.id, 10) + 1
+
+    return newId.toString()
+  }
+}
+
+const createCharacter = async (newCharacter) => {
+  const newId = await idCheck()
+
   return fetch(data, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(newCharacter),
-  })
+    body: JSON.stringify({ id: newId, ...newCharacter }),
+  }).then((resp) => resp.json())
 }
 
 export { createCharacter }
