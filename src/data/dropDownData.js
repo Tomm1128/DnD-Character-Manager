54c996970e8ad2a5354e011723de89e431b54b1f
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-03 10:33:40 -0400

    Restructure data file

---
+++ src/data/dropDownData.js
@@ -0,0 +1,39 @@
+const classes = [
+  "Barbarian",
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Fighter",
+  "Monk",
+  "Paladin",
+  "Ranger",
+  "Rogue",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+]
+
+const spellCastorClasses = [
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Paladin",
+  "Ranger",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+]
+
+const races = [
+  "Dwarf",
+  "Elf",
+  "Halfling",
+  "Human",
+  "Dragonborn",
+  "Gnome",
+  "Half-Elf",
+  "Half-Orc",
+  "Tiefling",
+]
+
+export { classes, spellCastorClasses, races }
