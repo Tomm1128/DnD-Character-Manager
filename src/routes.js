4ae69a3da8f9cdcc91f587cc30ba30ad88d6a267
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-03 11:47:30 -0400

    Add character view page to routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import App from "./App"
 import CreateCharacterPage from "./pages/CreateCharacterPage"
 import SpellDetailsPage from "./pages/SpellDetailsPage"
 import SpellsPage from "./pages/SpellsPage"
+import ViewCharacterPage from "./pages/ViewCharacterPage"
 
 const routes = [
   {
@@ -19,6 +20,10 @@ const routes = [
   {
     path: "/spells/:index",
     element: <SpellDetailsPage />,
+  },
+  {
+    path: "/view_character/:id",
+    element: <ViewCharacterPage />,
   },
 ]
 
