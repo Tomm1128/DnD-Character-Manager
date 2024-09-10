9cb87cc5484c50c6603f4b98903ff3364cfb6644
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-09-10 15:51:50 -0400

    Create character typr

---
+++ src/types/character.ts
@@ -0,0 +1,13 @@
+interface Character {
+  name: string;
+  race: string;
+  class: string;
+  level: number;
+  id: number;
+  height: string;
+  weight: string;
+  heightFeet: number;
+  heightInches: number;
+}
+
+export default Character
